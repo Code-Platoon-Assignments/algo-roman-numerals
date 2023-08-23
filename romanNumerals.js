d99bc1a27c54d5ee745074f3bd5d1894d2aa8026
Benjamin Cohen <benjamin@codeplatoon.org>
2023-08-23 13:13:51 -0600

    solution

--- romanNumerals.js
+++ romanNumerals.js
@@ -1,7 +1,73 @@
 export function toRomanLazy(num) {
-  return "";
+  let output = "";
+
+  const romanNumeralToArabic = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  const romanNumeralPriorityOrder = ["M", "D", "C", "L", "X", "V", "I"];
+
+  for (let romanNumeral of romanNumeralPriorityOrder) {
+    const arabicNum = romanNumeralToArabic[romanNumeral];
+    const numRoman = Math.floor(num / arabicNum);
+
+    for (let i = 0; i < numRoman; i++) {
+      output += romanNumeral;
+      num -= arabicNum;
+    }
+  }
+
+  return output;
 }
 
 export function toRoman(num) {
-  return "";
+  let output = "";
+
+  const romanNumeralToArabic = {
+    I: 1,
+    IV: 4,
+    V: 5,
+    IX: 9,
+    X: 10,
+    XL: 40,
+    L: 50,
+    C: 100,
+    CD: 400,
+    D: 500,
+    CM: 900,
+    M: 1000,
+  };
+
+  const romanNumeralPriorityOrder = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I",
+  ];
+
+  for (let romanNumeral of romanNumeralPriorityOrder) {
+    const arabicNum = romanNumeralToArabic[romanNumeral];
+    const numRoman = Math.floor(num / arabicNum);
+
+    for (let i = 0; i < numRoman; i++) {
+      output += romanNumeral;
+      num -= arabicNum;
+    }
+  }
+
+  return output;
 }
